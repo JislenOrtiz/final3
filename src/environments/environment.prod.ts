eba6ca2eac30625ee78a1409f22510c61f8a395c
jislen <jislen.ortiz@yahoo.com>
2021-01-18 22:51:43 -0600

    jisleeen

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC3IEMHoYVvGKFYmYIoCHcEmI4I1rsoL5o",
+    authDomain: "tienda-maquillaje-a8ee4.firebaseapp.com",
+    projectId: "tienda-maquillaje-a8ee4",
+    storageBucket: "tienda-maquillaje-a8ee4.appspot.com",
+    messagingSenderId: "880919097154",
+    appId: "1:880919097154:web:a20fcf7ca6603b21abe2db"
+  }
+};
